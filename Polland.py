a74f57bcd67d2a007ecc70cd59bb81e4c930f4bf
yassir elyasini <elyasiniyassir40@gmail.com>
2024-02-04 16:01:51 +0100

    update

--- Polland.py
+++ Polland.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-import math
-
-def mission(N):
-    last_num = N % 10
-    for i in range(2, 10):
-        if last_num % i == 0:
-            return i
-
-def check_sqrt(value):
-    if value.is_integer():
-        return 1
-    else:
-        return 0
 
 def factorize(number):
     for i in range(2, int(number**0.5) + 1):
